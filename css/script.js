0437ce5b8a0081ecf80bd2a01e02ca9e5cc08631
Nethajikumar <nethajikumar2002@gmail.com>
2025-02-18 09:57:01 +0530

    Add files via upload
    new one

---
+++ css/script.js
@@ -0,0 +1,9 @@
+// This script is optional for additional animations or effects
+document.addEventListener("DOMContentLoaded", () => {
+    // You can add further JavaScript animations here if you want
+});
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+  }
